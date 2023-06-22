1ef5e20f8e81595bb1f001d0887725c91f069954
sydneysugar <sydneysugar24@gmail.com>
2023-06-22 16:07:47 -0700

    Address PR template change

--- .github/scripts/ci_check.py
+++ .github/scripts/ci_check.py
@@ -1,7 +1,7 @@
 import os
 import re
 import json
-import requests
+import sys
 from github import Github
 
 def get_pull_request_number(pr_url):
@@ -29,8 +29,8 @@ def ci_check(pr_number, access_token):
     if 'internal' in labels:
          return True
         
-    # Check for text under 'External Release Notes'
-    release_notes_pattern = r'## External Release Notes(.*?)(?:(?=<!--- REPLACE THIS COMMENT WITH YOUR DESCRIPTION --->)|$)'
+    # Check for description of external change
+    release_notes_pattern = r'## External Release Notes\n(?!<!--- REPLACE THIS COMMENT WITH YOUR DESCRIPTION --->)(.*?)\S'
     release_notes_match = re.search(release_notes_pattern, description, re.DOTALL)
     if release_notes_match:
         release_notes_text = release_notes_match.group(1).strip()
@@ -40,51 +40,18 @@ def ci_check(pr_number, access_token):
     print('Pull requests must include a description in the release notes section.')
     return False
 
-def create_check_run(repo, pr_number, conclusion, output):
-    pr = repo.get_pull(pr_number)
-    commit_sha = pr.head.sha
-
-    check_run = repo.create_check_run(
-        name='CI Check',
-        head_sha=commit_sha,
-        status='completed',
-        conclusion=conclusion,
-        output=output
-    )
-
-def main():
+if __name__ == '__main__':
     event_path = os.environ['GITHUB_EVENT_PATH']
     with open(event_path, 'r') as f:
         event_payload = json.load(f)
         pr_number = event_payload['pull_request']['number']
-
+    
     access_token = os.environ['GITHUB_TOKEN']
 
-    result, error_message = ci_check(pr_number, access_token)
-
-    if result:
-        conclusion = 'success'
-        output = {
-            'title': 'CI Check Passed',
-            'summary': 'The CI check passed.',
-        }
-    else:
-        conclusion = 'failure'
-        output = {
-            'title': 'CI Check Failed',
-            'summary': error_message,
-        }
-
-    g = Github(access_token)
-    repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
-    create_check_run(repo, pr_number, conclusion, output)
+    result = ci_check(pr_number, access_token)
 
     if result:
         print('CI check passed.')
         exit(0)
     else:
-        print('CI check failed.')
         exit(1)
-
-if __name__ == '__main__':
-    main()
