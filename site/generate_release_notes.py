b8f7be94b7fe14b32daace5d80ef9d2bdf39513d
sydneysugar <sydneysugar24@gmail.com>
2023-07-04 11:12:23 -0700

    Fixed indentation and overwrite template

--- site/generate_release_notes.py
+++ site/generate_release_notes.py
@@ -5,28 +5,23 @@ import subprocess
 import json
 from datetime import datetime
 
-
 def get_tag_info():
   # Get the most recent tag name
   cmd_tag_name = ['gh', 'api', 'repos/validmind/documentation/tags', '--jq', '.[0].name']
   result_tag_name = subprocess.run(cmd_tag_name, capture_output=True, text=True)
   tag_name = result_tag_name.stdout.strip()
 
-
   # Get the commit SHA associated with the most recent tag
   cmd_commit_sha = ['gh', 'api', f'repos/validmind/documentation/git/matching-refs/tags/{tag_name}', '--jq', '.[].object.sha']
   result_commit_sha = subprocess.run(cmd_commit_sha, capture_output=True, text=True)
   tag_sha = result_commit_sha.stdout.strip()
-
 
   # Get the commit time from its SHA
   cmd_commit_time = ['gh', 'api', f'repos/validmind/documentation/git/commits/{tag_sha}', '--jq', '.committer.date']
   result_commit_time = subprocess.run(cmd_commit_time, capture_output=True, text=True)
   tag_time = result_commit_time.stdout.strip()[:10] # Truncate the date
 
-
   return tag_name, tag_time
-
 
 def get_merged_pull_requests():
   cmd = ['gh', 'pr', 'list', '--state', 'merged', '--json', 'title,number,body', '--limit', '30']
@@ -34,7 +29,6 @@ def get_merged_pull_requests():
   output = result.stdout.strip()
   pr_data = json.loads(output)
   return pr_data
-
 
 def get_labels(pull_request_number):
   cmd = ['gh', 'pr', 'view', str(pull_request_number), '--json', 'labels']
@@ -44,7 +38,6 @@ def get_labels(pull_request_number):
   labels = labels_data['labels']
   return labels
 
-
 def get_merged_at(pull_request_number):
   cmd = ['gh', 'pr', 'view', str(pull_request_number), '--json', 'mergedAt']
   result = subprocess.run(cmd, capture_output=True, text=True)
@@ -53,15 +46,13 @@ def get_merged_at(pull_request_number):
   merge_time = merge_time_data['mergedAt'][:10] # Truncate so only the date is printed
   return merge_time
 
-
 def extract_external_release_notes(pr_body):
   match = re.search(r"## External Release Notes(.+)", pr_body, re.DOTALL)
   if match:
       return match.group(1).strip()
   return None
 
-
-def generate_qmd_files(qmd_files, release_folder, formatted_date, merged_pull_requests, tag_time, release_date):
+def generate_qmd_files(qmd_files, release_folder, formatted_date, merged_pull_requests, tag_time, release_date, tag_name):
   for pr in merged_pull_requests:
       merged_at = get_merged_at(pr['number'])
       if merged_at >= tag_time:
@@ -73,7 +64,6 @@ def generate_qmd_files(qmd_files, release_folder, formatted_date, merged_pull_re
                       external_release_notes = extract_external_release_notes(pr['body'])
                       if external_release_notes:
                            qmd_files[label_name] += f"- **{pr['title']}**\n\n   {external_release_notes}\n\n\n"
-
 
   for label, release_notes in qmd_files.items():
       if release_notes:
@@ -87,105 +77,90 @@ def generate_qmd_files(qmd_files, release_folder, formatted_date, merged_pull_re
                file.write(f"## {label.capitalize()} -- {release_date}\n\n")
                file.write(release_notes)
 
-
   # Copy and rename release_highlights_template.qmd to the releases folder
   template_filename = "release_highlights_template.qmd"
   template_filepath = os.path.join(release_folder, f"release-notes-{formatted_date}.qmd")
   shutil.copyfile(template_filename, template_filepath)
+  with open(template_filepath, 'r') as file: 
+      template_content = file.read()
 
+  template_content = template_content.replace("Release date", release_date)
+  template_content = template_content.replace("version number", tag_name)
+
+  with open(template_filepath, 'w') as file:
+      file.write(template_content)
 
 def update_quarto_yaml(qmd_files, release_date):
   yaml_filename = "_quarto.yml"
   temp_yaml_filename = "_quarto_temp.yml"
 
-
   shutil.copyfile(yaml_filename, temp_yaml_filename)
-
 
   with open(temp_yaml_filename, 'r') as file:
       lines = file.readlines()
-
 
   with open(yaml_filename, 'w') as file:
       add_release_content = False
       insert_index = -1
 
-
       for i, line in enumerate(lines):
           file.write(line)
-
 
           if line.strip() == "- text: \"Releases\"":
               add_release_content = True
               insert_index = i + 1
 
-
           if add_release_content and i == insert_index:
               formatted_release_date = datetime.strptime(release_date, "%B %d, %Y").strftime("%Y-%b-%d").lower()
-              file.write(f'              - text: "{release_date}"\n')
-              file.write(f'                contents:\n')
-              file.write(f'                  - text: "Release Highlights"\n')
-              file.write(f'                    file: releases/{formatted_release_date}/release-notes-{formatted_release_date}.qmd\n')
-
+              file.write(f'            - text: "{release_date}"\n')
+              file.write(f'              contents:\n')
+              file.write(f'                - text: "Release Highlights"\n')
+              file.write(f'                  file: releases/{formatted_release_date}/release-notes-{formatted_release_date}.qmd\n')
 
               if qmd_files["enhancement"]:
-                  file.write(f'                  - text: "Enhancements"\n')
-                  file.write(f'                    file: releases/{formatted_release_date}/enhancement.qmd\n')
-
+                  file.write(f'                - text: "Enhancements"\n')
+                  file.write(f'                  file: releases/{formatted_release_date}/enhancement.qmd\n')
 
               if qmd_files["bug"]:
-                  file.write(f'                  - text: "Bug fixes"\n')
-                  file.write(f'                    file: releases/{formatted_release_date}/bug.qmd\n')
-
+                  file.write(f'                - text: "Bug fixes"\n')
+                  file.write(f'                  file: releases/{formatted_release_date}/bug.qmd\n')
 
               if qmd_files["deprecation"]:
-                  file.write(f'                  - text: "Deprecations"\n')
-                  file.write(f'                    file: releases/{formatted_release_date}/deprecation.qmd\n')
-
+                  file.write(f'                - text: "Deprecations"\n')
+                  file.write(f'                  file: releases/{formatted_release_date}/deprecation.qmd\n')
 
               if qmd_files["documentation"]:
-                  file.write(f'                  - text: "Documentation"\n')
-                  file.write(f'                    file: releases/{formatted_release_date}/documentation.qmd\n')
-
+                  file.write(f'                - text: "Documentation"\n')
+                  file.write(f'                  file: releases/{formatted_release_date}/documentation.qmd\n')
 
               add_release_content = False
 
-
   os.remove(temp_yaml_filename)
-
 
 if __name__ == '__main__':
   # Get the path of the cloned repository
   repo_path = os.path.join(os.getcwd())
 
-
   os.chdir(repo_path)  # Change the current working directory to the cloned repository path
-
 
   release_date = input("Enter the release date (Month Day, Year): ")
   formatted_release_date = datetime.strptime(release_date, "%B %d, %Y").strftime("%Y-%b-%d").lower()
 
-
   release_folder = os.path.join("releases", formatted_release_date)
-
 
   timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
   file_name = f"pr_data.txt"
   folder_path = f"releases/{formatted_release_date}"
   file_path = f"{folder_path}/{file_name}"
 
-
   # Create the new folder if it doesn't exist
   subprocess.run(['mkdir', '-p', folder_path])
-
 
   # Get the merged pull requests
   merged_pull_requests = get_merged_pull_requests()
 
-
   tag_name, tag_time = get_tag_info()
   print(f"Extracting from pull requests after release tag '{tag_name}'")
-
 
   with open(file_path, 'w') as file:
       file.write("Merged Pull Requests:\n")
@@ -203,9 +178,7 @@ if __name__ == '__main__':
           file.write(f"Body: {pr['body']}\n")
           file.write("\n")
 
-
   print(f"Pull request data saved to '{file_path}'.")
-
 
   qmd_files = {
       "highlight": "",
@@ -215,11 +188,8 @@ if __name__ == '__main__':
       "documentation": ""
   }
 
-
   os.makedirs(release_folder, exist_ok=True)
 
-
-  generate_qmd_files(qmd_files, release_folder, formatted_release_date, merged_pull_requests, tag_time, release_date)
-
+  generate_qmd_files(qmd_files, release_folder, formatted_release_date, merged_pull_requests, tag_time, release_date, tag_name)
 
   update_quarto_yaml(qmd_files, release_date)
