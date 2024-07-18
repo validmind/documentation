450fb308db0495d710b14580b6f754c63993979d
Nik Richers <nik@validmind.ai>
2024-07-17 19:51:48 -0700

    Remove extraneous files

--- site/_site/site_libs/quarto-contrib/nutshell-1.0.7/nutshell_options.js
+++
@@ -1,5 +0,0 @@
-Nutshell.setOptions({
-    startOnLoad: true, // Start Nutshell on load? (default: true)
-    lang: 'en', // Language (default: 'en', which is English)
-    dontEmbedHeadings: true, // If 'true', removes the "embed this as a nutshell" option on headings
-});
