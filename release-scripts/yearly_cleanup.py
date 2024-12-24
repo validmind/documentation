e2d5297cc060305ad5cbf6b65db9d1ba3c97470c
Beck <164545837+validbeck@users.noreply.github.com>
2024-12-24 11:08:57 -0800

    Create new year directory

--- release-scripts/yearly_cleanup.py
+++ release-scripts/yearly_cleanup.py
@@ -6,10 +6,10 @@ import numpy as np
 import datetime
 import openai
 from dotenv import dotenv_values
-import os
 from collections import defaultdict
 
 from datetime import datetime
+import os
 
 ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
 
@@ -28,9 +28,31 @@ def get_year():
         f"Enter the year you want to compile (leave empty for default [{default_year}]): "
     ) or default_year
 
-    print(f"Year inputted: {year}\n")
-    # return year
+    print(f"Year inputted:")
+    return year
 
+def create_year_folder(year):
+    """
+    Creates a new directory in ../site/releases/ with the given year as the name.
+
+    Args:
+        year (str): The name of the directory to create.
+
+    Returns:
+        str: The path to the new yearly directory.
+    """
+    releases_dir = f"../site/releases/"
+    yearly_path = f"../site/releases/{year}/"
+
+    os.makedirs(releases_dir, exist_ok=True)
+
+    if os.path.exists(yearly_path):
+        print(f"The directory '{yearly_path}' already exists.")
+    else:
+        os.makedirs(yearly_path)
+        print(f"Created folder: {yearly_path}")
+    
+    return yearly_path
 
 def main():
     get_year()
