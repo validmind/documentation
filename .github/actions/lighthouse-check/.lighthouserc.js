e9741d74b083ca5875cf31fd4212b52b5836f9d3
Nik Richers <nik@validmind.ai>
2025-05-05 08:30:43 -0700

    Experiment with better URL specification for reports

--- .github/actions/lighthouse-check/.lighthouserc.js
+++ .github/actions/lighthouse-check/.lighthouserc.js
@@ -1,17 +1,9 @@
+const fs = require('fs');
 module.exports = {
   ci: {
     collect: {
       staticDistDir: 'site/_site',
-      url: [
-        'http://localhost:8080/index.html',
-        'http://localhost:8080/get-started/get-started.html',
-        'http://localhost:8080/guide/guides.html',
-        'http://localhost:8080/developer/validmind-library.html',
-        'http://localhost:8080/support/support.html',
-        'http://localhost:8080/releases/all-releases.html',
-        'http://localhost:8080/training/training.html',
-        // Add more URLs as needed
-      ],
+      url: fs.readFileSync('lhci-urls.txt', 'utf-8').split('\n').filter(Boolean),
     },
     assert: {
       assertions: {
