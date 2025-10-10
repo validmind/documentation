dc303ec82d16d7b406ab6cdc8fae451bcb03e17b
Nik Richers <nik@validmind.ai>
2025-10-10 10:07:07 -0700

    Upgrade Tachyons with official codeblock fix & remove our workaround

---
+++ site/_extensions/gadenbuie/tachyons/tachyons.lua
@@ -0,0 +1,9 @@
+function Pandoc()
+  if quarto.doc.is_format("html:js") then
+    quarto.doc.add_html_dependency({
+      name = 'tachyons',
+      version = '4.12.0',
+      stylesheets = { "tachyons.css", "quarto-resets.css" },
+    })
+  end
+end
--- site/_extensions/nareal/tachyons/tachyons.lua
+++
@@ -1,8 +0,0 @@
-
-function Pandoc(doc)
-  quarto.doc.add_html_dependency({
-    name = "tachyons",
-    version = "4.12.0",
-    stylesheets = { "css/tachyons.min.css" }
-  })
-end
