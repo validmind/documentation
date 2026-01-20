be802f2bae72a0af4c92b6052bd390b0780ff9b5
Nik Richers <nik@validmind.ai>
2026-01-20 11:19:04 -0800

    Enable custom URLs, take 2 (#1120)
    * Add initial custom URL configuration button
    
    * Remove temporary file
    
    * Move new CSS into slides.scss
    
    * Explicitly state format for non-revealjs files to avoid double outputs
    
    * Move include-in-header directive into revealjs files
    
    * Remove Latinism, make sampe URL more generic
    
    * Fix placeholder
    
    * Update site/training/assets/vmurl-settings.js
    
    Co-authored-by: Beck <164545837+validbeck@users.noreply.github.com>
    
    * Update site/training/assets/vmurl-settings.js
    
    Co-authored-by: Beck <164545837+validbeck@users.noreply.github.com>
    
    * Address review comments
    
    * Button text & alignment
    
    ---------
    
    Co-authored-by: Beck <164545837+validbeck@users.noreply.github.com>

--- site/training/assets/vmurl-settings.js
+++ site/training/assets/vmurl-settings.js
@@ -71,16 +71,16 @@
     modal.innerHTML = `
       <div class="vmurl-modal">
         <div class="vmurl-modal-header">
-          <h3>ValidMind URL settings</h3>
+          <h3>Customize Your ValidMind Platform URL</h3>
           <button class="vmurl-close-btn" aria-label="Close">&times;</button>
         </div>
         <div class="vmurl-modal-body">
+          <p class="vmurl-hint">If you have been provided with a custom URL to access our platform, enter it here. Leave blank to use the default.</p><p>&nbsp;</p>
           <p class="vmurl-current">Current: <code>${currentUrl}</code>${customUrl ? ' (custom)' : ' (default)'}</p>
-          <label for="vmurl-input">Custom ValidMind URL:</label>
+          <label for="vmurl-input">ValidMind Platform URL</label>
           <input type="url" id="vmurl-input" class="vmurl-input" 
                  placeholder="https://myurl.validmind.ai" 
                  value="${customUrl || ''}">
-          <p class="vmurl-hint">Enter the base URL for the ValidMind platform. Leave empty to use the default.</p>
         </div>
         <div class="vmurl-modal-footer">
           <button class="vmurl-btn vmurl-btn-secondary" id="vmurl-reset">Reset to Default</button>
@@ -146,7 +146,7 @@
       btn.className = 'vmurl-settings-btn';
       btn.setAttribute('aria-label', 'Configure ValidMind URL');
       btn.setAttribute('title', 'Configure ValidMind URL');
-      btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="16" height="16"><path fill="currentColor" d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>';
+      btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12" height="12"><path fill="currentColor" d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>Custom URL';
       
       btn.addEventListener('click', function(e) {
         e.preventDefault();
