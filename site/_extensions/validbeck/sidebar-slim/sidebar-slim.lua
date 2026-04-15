77657dd8fc7ed97cb2986650fc1e60ea4fded206
Beck <164545837+validbeck@users.noreply.github.com>
2026-04-15 16:02:53 -0700

    Edit

--- site/_extensions/validbeck/sidebar-slim/sidebar-slim.lua
+++ site/_extensions/validbeck/sidebar-slim/sidebar-slim.lua
@@ -44,7 +44,7 @@ function Pandoc(doc)
 
   quarto.doc.add_html_dependency({
     name = "validbeck-sidebar-slim",
-    version = "1.0.3",
+    version = "1.0.4",
     stylesheets = { "sidebar-slim.css" },
     scripts = { "sidebar-slim.js" },
   })
